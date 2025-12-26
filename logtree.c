b2c7fbafab53b48d6d272be4d0975d41832aeaa8
Caio Lima <eusoucaioemanuel@gmail.com>
2025-12-26 12:31:18 -0300

    Alterações para completar os arquivos "fila.c", "logtree.c" e "logtree.h"

--- logtree.c
+++ logtree.c
@@ -11,10 +11,12 @@ void log_inicializar(Log **l){
 // “classe”, que esperou “timer” minutos para ser atendido pelo caixa de número “caixa”, e levou “tempo”
 // minutos sendo atendido.
 void log_registrar(Log **l, int conta, int classe, int timer, int caixa){
-    Log * node = *l;
-    Log * registrar = (Log*) malloc(sizeof(Log));
-    Log * pai = NULL;
 
+    Log * node = *l;    // Nó que será usado para percorrer a árvore
+    Log * registrar = (Log*) malloc(sizeof(Log));   // Nó que será usado para o novo registro
+    Log * pai = NULL;   // Nó para referenciar o pai do novo registro na hora da inserção
+
+    // Atribuições necessárias
     registrar->conta = conta;
     registrar->classe = classe;
     registrar->tempo = timer;
@@ -22,6 +24,7 @@ void log_registrar(Log **l, int conta, int classe, int timer, int caixa){
     registrar->dir = NULL;
     registrar->esq = NULL;
 
+    // Percorre a árvore procurando onde inserir esse novo registro
     while(node != NULL){
         pai = node;
         if(conta > node->conta)
@@ -30,8 +33,12 @@ void log_registrar(Log **l, int conta, int classe, int timer, int caixa){
             node = node->esq;
     }
 
+    // Se o pai é nulo, então a árvore é nula. Portanto, 'l' será o novo registro
     if(pai == NULL)
         *l = registrar;
+    
+    /* Se o pai não for nulo mas o nó for nulo, veremos se o novo registro 
+    será inserido na esquerda ou na direita do pai.*/
     else if(conta > pai->conta)
         pai->dir = registrar;
     else
@@ -39,7 +46,7 @@ void log_registrar(Log **l, int conta, int classe, int timer, int caixa){
 }
 
 // Retorna o tempo médio de espera, em minutos, para a classe de número “classe”.
-float log_media_por_classe(Log *l, int classe) {
+float log_media_por_classe(Log **l, int classe) {
     float soma = 0.0;
     int qtd = 0;
 
@@ -55,24 +62,67 @@ float log_media_por_classe(Log *l, int classe) {
 }
 
 // Função auxiliar recursiva para percorrer e somar
-void auxiliar_media(Log *node, int classe, float *soma, int *qtd) {
+void auxiliar_media(Log **node, int classe, float *soma, int *qtd) {
+    if (*node == NULL) {
+        return;
+    }
+
+    // Se a classe bater, acumula o tempo e incrementa o contador
+    if ((*node)->classe == classe) {
+        *soma += (*node)->tempo;
+        *qtd += 1;
+    }
+
+    // Continua percorrendo para esquerda e direita
+    auxiliar_media((*node)->esq, classe, soma, qtd);
+    auxiliar_media((*node)->dir, classe, soma, qtd);
+}
+
+// Retorna a soma dos tempos de espera de todos os clientes cujas contas fazem parte da classe de número “classe”.
+int log_obter_soma_por_classe(Log **l, int classe) {
+    int soma = 0;
+
+    // Chama a auxiliar passando os endereços das variáveis
+    auxiliar_soma(l, classe, &soma);
+
+    return soma;
+}
+
+// Função auxiliar recursiva para percorrer e somar os tempos
+void auxiliar_soma(Log **node, int classe, int *soma) {
     if (node == NULL) {
         return;
     }
 
     // Se a classe bater, acumula o tempo e incrementa o contador
-    if (node->classe == classe) {
-        *soma += node->tempo;
-        *qtd += 1;
+    if ((*node)->classe == classe) {
+        *soma += (*node)->tempo;
     }
 
     // Continua percorrendo para esquerda e direita
-    auxiliar_media(node->esq, classe, soma, qtd);
-    auxiliar_media(node->dir, classe, soma, qtd);
+    auxiliar_soma((*node)->esq, classe, soma);
+    auxiliar_soma((*node)->dir, classe, soma);
 }
 
-// Retorna a soma dos tempos de espera de todos os clientes cujas contas fazem parte da classe de número “classe”.
-int log_obter_soma_por_classe(Log **l, int classe);
-
 // Retorna a quantidade de clientes atendidos cujas contas são da categoria de número “classe”.
-int log_obter_contagem_por_classe(Log **l, int classe);
+int log_obter_contagem_por_classe(Log **l, int classe) {
+    int soma=0;
+    auxiliar_soma_qtde(l, classe, &soma);
+    return soma;
+}
+
+// Função auxiliar recursiva para percorrer e somar a quantidade de clientes da classe "classe"
+void auxiliar_soma_qtde(Log **node, int classe, int *soma) {
+    if (node == NULL) {
+        return;
+    }
+
+    // Se a classe bater, acumula o tempo e incrementa o contador
+    if ((*node)->classe == classe) {
+        *soma += 1;
+    }
+
+    // Continua percorrendo para esquerda e direita
+    auxiliar_soma((*node)->esq, classe, soma);
+    auxiliar_soma((*node)->dir, classe, soma);
+}
--- logtree.h
+++ logtree.h
@@ -11,10 +11,14 @@ void log_inicializar(Log **l);
 
 void log_registrar(Log **l, int conta, int classe, int timer, int caixa);
 
-float log_media_por_classe(Log *l, int classe);
+float log_media_por_classe(Log **l, int classe);
 
-void auxiliar_media(Log *node, int classe, float *soma, int *qtd);
+void auxiliar_media(Log **node, int classe, float *soma, int *qtd);
 
 int log_obter_soma_por_classe(Log **l, int classe);
 
+void auxiliar_soma(Log **node, int classe, int *soma);
+
 int log_obter_contagem_por_classe(Log **l, int classe);
+
+void auxiliar_soma_qtde(Log **node, int classe, int *soma);
